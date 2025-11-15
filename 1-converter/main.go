707129428306e1352eff9f80d1c1526383a4ca1d
Vladislav <z3208z@ya.ru>
2025-11-15 19:37:43 +0300

    3 дз на проверку поправил main в соответствии с требованиями дз3

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -6,10 +6,6 @@ import (
 	"strings"
 )
 
-const usd2rub = 81.29
-const eur2usd = 1.16
-const eur2rub = 94.19
-
 func main() {
 
 	result := work()
@@ -91,34 +87,25 @@ func valCheckInt(x int) (int, error) {
 }
 
 func work() float64 {
+
+	convertRub := map[string]float64{"usd": 0.0123266, "eur": 0.010515}
+	convertEur := map[string]float64{"usd": 1.16, "rub": 95.1}
+	convertUsd := map[string]float64{"usd": 0.8602, "rub": 81.13}
+
 	inputVal1, inputVal2, inputVal3 := getUserInput()
 	inputVal4 := float64(inputVal2)
 	var result float64
 
 	switch inputVal1 {
 	case "rub":
-		switch inputVal3 {
-		case "eur":
-			result = inputVal4 / eur2rub
-		case "usd":
-			result = inputVal4 / usd2rub
-		}
+
+		result = inputVal4 * convertRub[inputVal3]
 
 	case "eur":
-		switch inputVal3 {
-		case "rub":
-			result = inputVal4 * eur2rub
-		case "usd":
-			result = inputVal4 / eur2usd
-		}
+		result = inputVal4 * convertEur[inputVal3]
 
 	case "usd":
-		switch inputVal3 {
-		case "rub":
-			result = inputVal4 * usd2rub
-		case "eur":
-			result = inputVal4 * eur2usd
-		}
+		result = inputVal4 * convertUsd[inputVal3]
 
 	}
 	return result
