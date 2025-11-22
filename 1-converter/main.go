2c038c42d76919130a82c43bbe86f88b025f5a18
Vladislav <z3208z@ya.ru>
2025-11-22 19:20:16 +0300

    3 дз на проверку добавил указатели

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -8,7 +8,11 @@ import (
 
 func main() {
 
-	res := work()
+	convertRub := map[string]float64{"usd": 0.0123266, "eur": 0.010515}
+	convertEur := map[string]float64{"usd": 1.16, "rub": 95.1}
+	convertUsd := map[string]float64{"usd": 0.8602, "rub": 81.13}
+
+	res := work(&convertRub, &convertEur, &convertUsd)
 	fmt.Println("результат", *res)
 }
 
@@ -86,17 +90,13 @@ func valCheckInt(x int) (int, error) {
 	}
 }
 
-func work() *float64 {
+func work(convertRub, convertEur, convertUsd *map[string]float64) *float64 {
 
 	inputVal1, inputVal2, inputVal3 := getUserInput()
 	inputVal4 := float64(inputVal2)
 	var result float64
 
-	convertRub := map[string]float64{"usd": 0.0123266, "eur": 0.010515}
-	convertEur := map[string]float64{"usd": 1.16, "rub": 95.1}
-	convertUsd := map[string]float64{"usd": 0.8602, "rub": 81.13}
-
-	convertFrom := map[string]map[string]float64{"rub": convertRub, "eur": convertEur, "usd": convertUsd}
+	convertFrom := map[string]map[string]float64{"rub": *convertRub, "eur": *convertEur, "usd": *convertUsd}
 	result = convertFrom[inputVal1][inputVal3] * inputVal4
 
 	return &result
